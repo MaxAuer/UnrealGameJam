0d7425ca0d8c23b56d1df78f886c8cc164425ad8
Max Auer <maxauer@hotmail.de>
2017-06-30 18:19:45 +0200

    Unreal C++ Code Project
    initial commit
    basic project setup

---
+++ BlessingInDisguise/Source/BlessingInDisguise/BlessingInDisguiseGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlessingInDisguiseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLESSINGINDISGUISE_API ABlessingInDisguiseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
